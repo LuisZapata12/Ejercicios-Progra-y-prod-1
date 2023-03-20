626a4d462b7b95f2f21164a78e42571025427d72
LuisZapata12 <luis.98.30.16@gmail.com>
2023-03-20 18:48:11 -0500

    update

--- Herencia.cs
+++ Herencia.cs
@@ -1,19 +1,16 @@
 
 using System;
 
-
+public class Padre
+{
+    public int a;
+    public int b;
+}
 public class Herencia : Padre
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+  
         Console.WriteLine("herede de padre " + a);
         Console.WriteLine("herede de padre " + b);
-    }
+    
 }
+
